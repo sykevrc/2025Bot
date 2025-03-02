7974952c19409a1a061d130f82b4dda840d6fac0
Johnson Wei <98916519+sykevrc@users.noreply.github.com>
2025-03-02 11:09:13 -0600

    Update ElevatorSubsystem.java to Kraken

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -3,24 +3,6 @@ package frc.robot.subsystems;
 import java.util.EnumSet;
 import java.util.Map;
 
-import com.revrobotics.sim.SparkMaxSim;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.SparkBase;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
-import com.revrobotics.spark.config.MAXMotionConfig.MAXMotionPositionMode;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
-
-import edu.wpi.first.math.controller.ElevatorFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
@@ -43,36 +25,48 @@ public class ElevatorSubsystem extends SubsystemBase {
         AlgaeL2,
         AlgaeL1,
         AlgaeShoot,
-        AlgaeFloor,
-        ClimberUp,
-        ClimberDown
+        AlgaeFloor
     }
 
-    private boolean isSim = false;
-    private ElevatorState state = ElevatorState.Start;
-    private double targetPosition = 0.0;
-    private SparkMax motor = null;
-    private SparkMaxSim motorSim = null;
-    private SparkMax motor2 = null;
-    private SparkMaxSim motor2Sim = null;
-    private SparkClosedLoopController pid = null;
-    /*private ProfiledPIDController profiledPIDController = new ProfiledPIDController(
-        0.1,
-        0.0, 
-        0.1, 
-        new TrapezoidProfile.Constraints(2, 2),
-        0.02
-    );*/ 
-    private SparkMaxConfig config = new SparkMaxConfig();
-    private double p = Constants.ElevatorConstants.P;
-    private double i = Constants.ElevatorConstants.I;
-    private double d = Constants.ElevatorConstants.D;
-    // these values were calculate using https://www.reca.lc/linear
-    private ElevatorFeedforward elevatorFeedforward = new ElevatorFeedforward(1.1, 1.55, 3.07);
+private boolean isSim = false;
+private final TalonFX m_Kraken;
+private final DutyCycleOut m_DriverOutput;
+private double currentPosition = 0.0;
+private double targetPosition = 0.0;
+// Initialize objects
+m_Kraken = new TalonFX(Constants.TALON_CAN_ID);
+m_DriverOutput = new DutyCycleOut(0); // Initialize with 0% output
 
-    private double currentPosition = 0.0;
-    private double previousValue = 0.0;
-    private int revolutionCount = 0;
+// start with factory-default configs
+var currentConfigs = new MotorOutputConfigs();
+
+// Set direction of rotation
+currentConfigs.Inverted = InvertedValue.Clockwise_Positive;
+
+// Apply configuration settings to motor driver
+m_Kraken.getConfigurator().apply(currentConfigs);
+
+// Initialize position encoder
+m_Kraken.setPosition(0);
+
+var talonFXConfigs = new TalonFXConfiguration();
+
+// set slot 0 gains
+var slot0Configs = talonFXConfigs.Slot0;
+slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
+slot0Configs.kI = 0; // no output for integrated error
+slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+
+// set Motion Magic settings
+var motionMagicConfigs = talonFXConfigs.MotionMagic;
+motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
+motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
+motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
+
+m_Kraken.getConfigurator().apply(talonFXConfigs);
 
     public ElevatorSubsystem() {
 
@@ -83,20 +77,6 @@ public class ElevatorSubsystem extends SubsystemBase {
             } else {
                 isSim = true;
             }
-
-            motor = new SparkMax(Constants.ElevatorConstants.motor_id, MotorType.kBrushless);
-            motor2 = new SparkMax(Constants.ElevatorConstants.motor2_id, MotorType.kBrushless);
-
-            if(isSim) {
-                motorSim = new SparkMaxSim(motor, DCMotor.getNEO(1));
-                motor2Sim = new SparkMaxSim(motor2, DCMotor.getNEO(1));
-            }
-
-            setConfig();
-
-            pid = motor.getClosedLoopController();
-
-            
 
             if (Constants.kEnableDebugElevator) {
 
@@ -120,11 +100,6 @@ public class ElevatorSubsystem extends SubsystemBase {
             // just returning to save cycles
             return;
         }
-
-        // Are we sending the same state again?  If so act like a toggle and stop
-        //if (this.state == state) {
-        //    targetPosition = Constants.SliderConstants.Stopped;
-        //} else {
 
             switch (state) {
                 case Start:
@@ -163,12 +138,6 @@ public class ElevatorSubsystem extends SubsystemBase {
                 case AlgaeFloor:
                     targetPosition = Constants.ElevatorConstants.AlgaeFloor;
                     break;
-                case ClimberUp:
-                    targetPosition = Constants.ElevatorConstants.ClimberUp;
-                    break;
-                case ClimberDown:
-                    targetPosition = Constants.ElevatorConstants.ClimberDown;
-                    break;
                 default:
                     targetPosition = 0.0;
                     break;
@@ -186,92 +155,16 @@ public class ElevatorSubsystem extends SubsystemBase {
 	public void periodic() {
 
         if (Constants.kEnableElevator) {
-            pid.setReference(targetPosition, ControlType.kPosition);
-            //pid.setReference(targetPosition, SparkBase.ControlType.kMAXMotionPositionControl);
-
-            // Try to do a setReference using a Feed Forward
-            /*pid.setReference(
-                targetPosition,
-                ControlType.kPosition,
-                ClosedLoopSlot.kSlot0,
-                elevatorFeedforward.calculate(
-                    motor.getEncoder().getVelocity()
-                )
-            );*/
-
-            /*motor.set(
-                profiledPIDController.calculate(motor.getAbsoluteEncoder().getPosition(), targetPosition)
-            );*/
-
-
-            if (isSim) {
-                motorSim.iterate(
-                        // 0.1,
-                        // desiredState.speedMetersPerSecond,
-                        motor.getOutputCurrent(),
-                        RoboRioSim.getVInVoltage(), // Simulated battery voltage, in Volts
-                        0.02
-                    );
-            }
+           	// create a Motion Magic request, voltage output
+		final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
+		
+		// set target position 
+		m_Kraken.setControl(m_request.withPosition(targetPosition));
         }
     }
 
     private void setConfig() {
-        // Vortex
-        config = new SparkMaxConfig();
-
-        config
-            //.inverted(false) // bore encoder testing
-            .inverted(true)
-            //.smartCurrentLimit(200)
-            .idleMode(IdleMode.kCoast);
-        //config.encoder
-            //.positionConversionFactor(25)
-            //.velocityConversionFactor(25);
-
-        config.closedLoop
-            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-            //.feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-            //.feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder)
-            //.feedbackSensor(FeedbackSensor.kAnalogSensor)
-            //.positionWrappingEnabled(true)
-		    .pid(
-			    p, 
-                i, 
-                d
-			);//.outputRange(-1, 1);
-
-        // Set MAXMotion parameters
-        config.closedLoop.maxMotion
-            //.maxVelocity(6784)
-            .maxVelocity(100)
-            .maxAcceleration(50)
-            //.positionMode(MAXMotionPositionMode.kMAXMotionTrapezoidal)
-            .allowedClosedLoopError(.05);
-
-        config.signals.primaryEncoderPositionPeriodMs(5);
-        config.signals.primaryEncoderPositionAlwaysOn(true);
-
-        motor.configure(
-		    config, 
-			ResetMode.kResetSafeParameters, 
-			PersistMode.kPersistParameters
-		);
-
-        SparkMaxConfig config2 = new SparkMaxConfig();
-
-        config2
-            .idleMode(IdleMode.kCoast);
-        config2.follow(Constants.ElevatorConstants.motor_id);
-
-        motor2.configure(
-            config2, 
-            ResetMode.kResetSafeParameters, 
-            PersistMode.kPersistParameters
-        );
-
-        /*profiledPIDController.setPID(p, i, d);
-        profiledPIDController.setTolerance(0.05);*/
+        
     }
 
     public double getPosition() {
@@ -280,19 +173,6 @@ public class ElevatorSubsystem extends SubsystemBase {
             return motorSim.getRelativeEncoderSim().getPosition();
         }
 
-        return motor.getEncoder().getPosition();
-        //currentPosition = motor.getEncoder().getPosition();
-        /*currentPosition = motor.getAbsoluteEncoder().getPosition();
-
-        if(currentPosition < .2 && previousValue > .8) {
-            revolutionCount++;
-        } else if(currentPosition > .8 && previousValue < .2) {
-            revolutionCount--;
-        }
-
-        previousValue = currentPosition;
-
-        return revolutionCount + currentPosition;*/
     }
 
     public double getTargetPosition() {
@@ -303,58 +183,33 @@ public class ElevatorSubsystem extends SubsystemBase {
     public void setTargetPosition(double targetPosition) {
         this.targetPosition = targetPosition;
     }
-
-    public boolean atTargetPosition() {
-        // return true if we are just about at the target position
-        return Math.abs(targetPosition - currentPosition) < 0.05;
+public double getJerk() {
+        return this.motionMagicConfigs.MotionMagicJerk;
     }
-
-    public double getP() {
-        return this.p;
+public double getCruise() {
+        return this.motionMagicConfigs.MotionMagicCruiseVelocity;
     }
-
-    public void setP(double p) {
-        this.p = p;
-        setConfig();
+public double getP() {
+        return this.slot0Configs.kP;
     }
-
-    public double getI() {
-        return this.i;
+public void setJerk(double jerk) {
+        this.motionMagicConfigs.MotionMagicJerk = jerk;
     }
-
-    public void setI(double i) {
-        this.i = i;
-        setConfig();
+public void setCruise(double cruise) {
+        this.motionMagicConfigs.MotionMagicCruiseVelocity = cruise;
     }
-
-    public double getD() {
-        return this.d;
+public void setP(double p) {
+        this.slot0Configs.kP = p;
     }
-
-    public void setD(double d) {
-        this.d = d;
-        setConfig();
-    }
-
-    public int getRevolutions() {
-        return this.revolutionCount;
-    }
-
-    /*public boolean atTargetPosition() {
-        return profiledPIDController.atSetpoint();
-    }*/
-
     @Override
     public void initSendable(SendableBuilder builder) {
         builder.setSmartDashboardType("RobotPreferences");
         //builder.setActuator(true);
         //builder.setSafeState(this::disable);
-        builder.addDoubleProperty("D", this::getD, this::setD);
-        builder.addDoubleProperty("I", this::getI, this::setI);
+        builder.addDoubleProperty("Jerk", this::getJerk, this::setJerk);
+        builder.addDoubleProperty("Cruise", this::getCruise, this::setCruise);
         builder.addDoubleProperty("P", this::getP, this::setP);
         builder.addDoubleProperty("Target", this::getTargetPosition, this::setTargetPosition);
         builder.addDoubleProperty("Position", this::getPosition,null);
-        builder.addBooleanProperty("At Target Position", this::atTargetPosition,null);
-        builder.addDoubleProperty("Revolutions", this::getRevolutions,null);
     }
 }
