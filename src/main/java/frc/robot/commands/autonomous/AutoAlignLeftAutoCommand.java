22cb130c27c3d2db43e508bd3322b146ad8acd9f
leal.philip <leal.philip@heb.com>
2025-03-14 08:56:11 -0500

    fixing autoalignauto

--- src/main/java/frc/robot/commands/autonomous/AutoAlignLeftAutoCommand.java
+++ src/main/java/frc/robot/commands/autonomous/AutoAlignLeftAutoCommand.java
@@ -1,9 +1,12 @@
 package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.LimelightHelpers;
 import frc.robot.RobotContainer;
+import frc.robot.commands.AutoAlignLeftCommand;
 import frc.robot.mechanisms.LED.LEDStatus;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ArmSubsystem.ArmState;
@@ -13,121 +16,31 @@ import frc.robot.subsystems.EndEffectorSubsystem.EndEffectorState;
 import frc.robot.tools.Limelight;
 
 public class AutoAlignLeftAutoCommand extends Command {
-    private DriveSubsystem driveSubsystem;
-    private EndEffectorSubsystem endEffectorSubsystem;
-    private ArmSubsystem armSubsystem;
-    private Limelight limelight;
+
     private boolean finished = false;
-    private double aprilTagLocation = 0.0;
-    private double aprilTagLocationY = 0.0;
+    private AutoAlignLeftCommand autoAlignLeftCommand;
+    private SequentialCommandGroup scg;
+
 
     public AutoAlignLeftAutoCommand() {
-        this.driveSubsystem = RobotContainer.driveSubsystem;
-        this.endEffectorSubsystem = RobotContainer.endEffectorSubsystem;
-        this.limelight = RobotContainer.limelight;
-        this.armSubsystem = RobotContainer.armSubsystem;
-        addRequirements(driveSubsystem);
-        addRequirements(endEffectorSubsystem);
-        addRequirements(armSubsystem);
+        
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        System.out.println("AutoAlignLeftAutoCommand::initialize() called");
-        finished = true;
+        finished = false;
+        autoAlignLeftCommand = new AutoAlignLeftCommand();
+        scg = new SequentialCommandGroup(autoAlignLeftCommand);
+        scg.schedule();
+        
+        //CommandScheduler.getInstance().schedule(scg);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-
-        if(limelight.hasTarget()) {
-            aprilTagLocation = LimelightHelpers.getTX(Constants.LimelightConstants.name);            
-
-            if(
-                 //14 > 20 - 3
-                 //14 < 20 + 3
-                //(-aprilTagLocation ) > (Constants.DriveConstants.kAutoAlignOffset - Constants.DriveConstants.kAutoAlignTolerance)
-                //&& (-aprilTagLocation) < (Constants.DriveConstants.kAutoAlignOffset + Constants.DriveConstants.kAutoAlignTolerance)
-                (-aprilTagLocation) > 13 && (-aprilTagLocation) < 15
-            ) {
-                // We are in the zone
-                driveSubsystem.driveRobotRelative(0.0, 0.0, 0.0);
-
-                /*SequentialCommandGroup scg = new SequentialCommandGroup(
-                    new EjectCoralCommand(),
-                    new DriveBackwardsCommand(),
-                    new ArmStartCommand()
-
-                );
-
-                scg.execute();*/
-
-                // Set the LED to show that it has the target
-                RobotContainer.led1.setStatus(LEDStatus.targetAquired);
-                
-                if(
-                    armSubsystem.getDesiredState() == ArmState.CoralL1
-                    || armSubsystem.getDesiredState() == ArmState.CoralL2
-                ) {
-                    // We are in a CoralL1 or CoralL2 position, eject out the front
-                    endEffectorSubsystem.setDesiredState(EndEffectorState.EjectCoralFront);
-                    finished = true;
-                } else if(
-                    armSubsystem.getDesiredState() == ArmState.CoralL3
-                    || armSubsystem.getDesiredState() == ArmState.CoralL4
-                ) {
-                    // We are in a CoralL3 or CoralL4 position, eject out the back
-                    endEffectorSubsystem.setDesiredState(EndEffectorState.EjectCoralBack);
-                    finished = true;
-                }
-            } else if(
-                (aprilTagLocation) < (-Constants.DriveConstants.kAutoAlignOffset + Constants.DriveConstants.kAutoAlignTolerance)
-            ) {
-                driveSubsystem.driveRobotRelative(0.0, -Constants.DriveConstants.kAutoAlignSpeed, 0.0);
-
-                // Set the LED to show that it has the target
-                RobotContainer.led1.setStatus(LEDStatus.targetSearching);
-            } else if (
-                (aprilTagLocation) > (-Constants.DriveConstants.kAutoAlignOffset - Constants.DriveConstants.kAutoAlignTolerance)
-            ) {
-                driveSubsystem.driveRobotRelative(0.0, Constants.DriveConstants.kAutoAlignSpeed, 0.0);
-
-                // Set the LED to show that it has the target
-                RobotContainer.led1.setStatus(LEDStatus.targetSearching);
-            }
-
-            // align in the y axis
-            // aprilTagLocationY = LimelightHelpers.getTY(Constants.LimelightConstants.name);
-            // if(
-            //     (aprilTagLocationY ) > (-Constants.DriveConstants.kAutoAlignLeftYOffset - Constants.DriveConstants.kAutoAlignLeftYTolerance)
-            //     && (aprilTagLocationY) < (-Constants.DriveConstants.kAutoAlignLeftYOffset + Constants.DriveConstants.kAutoAlignLeftYTolerance)
-            // ) {
-            //     System.out.println("AutoAlignLeftCommand::execute() - we are at the correct distance");
-            //     // we are now in the correct distance
-            //     //driveSubsystem.driveRobotRelative(0.0, 0.0, 0.0);
-            //     /*if(
-            //         armSubsystem.getDesiredState() == ArmState.CoralL1
-            //         || armSubsystem.getDesiredState() == ArmState.CoralL2
-            //     ) {
-            //         // We are in a CoralL1 or CoralL2 position, eject out the front
-            //         endEffectorSubsystem.setDesiredState(EndEffectorState.EjectCoralFront);
-            //     } else if(
-            //         armSubsystem.getDesiredState() == ArmState.CoralL3
-            //         || armSubsystem.getDesiredState() == ArmState.CoralL4
-            //     ) {
-            //         // We are in a CoralL3 or CoralL4 position, eject out the back
-            //         endEffectorSubsystem.setDesiredState(EndEffectorState.EjectCoralBack);
-            //     }*/
-            // }
-        } else {
-            // we don't have a target to stop
-            driveSubsystem.driveRobotRelative(0.0, 0.0, 0.0);
-
-            // Set the LED to show that it has the target
-            RobotContainer.led1.setStatus(LEDStatus.targetSearching);
-        }
+        scg.execute();
     }
 
     // Called once the command ends or is interrupted.
@@ -137,7 +50,6 @@ public class AutoAlignLeftAutoCommand extends Command {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        System.out.println("AutoAlignLeftAutoCommand::isFinished() called");
-        return finished;
+        return scg.isFinished();
     }
 }
--- src/main/java/frc/robot/commands/autonomous/AutoAlignRightAutoCommand.java
+++ src/main/java/frc/robot/commands/autonomous/AutoAlignRightAutoCommand.java
@@ -1,9 +1,12 @@
 package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.LimelightHelpers;
 import frc.robot.RobotContainer;
+import frc.robot.commands.AutoAlignLeftCommand;
+import frc.robot.commands.AutoAlignRightCommand;
 import frc.robot.mechanisms.LED.LEDStatus;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ArmSubsystem.ArmState;
@@ -14,95 +17,30 @@ import frc.robot.tools.Limelight;
 
 public class AutoAlignRightAutoCommand extends Command  {
 
-    private DriveSubsystem driveSubsystem;
-    private EndEffectorSubsystem endEffectorSubsystem;
-    private ArmSubsystem armSubsystem;
-    Limelight limelight;
     private boolean finished = false;
-    double aprilTagLocation = 0.0;
+    private AutoAlignRightCommand autoAlignRightCommand;
+    private SequentialCommandGroup scg;
     
     public AutoAlignRightAutoCommand() {
-        this.driveSubsystem = RobotContainer.driveSubsystem;
-        this.endEffectorSubsystem = RobotContainer.endEffectorSubsystem;
-        this.limelight = RobotContainer.limelight;
-        this.armSubsystem = RobotContainer.armSubsystem;
-        addRequirements(driveSubsystem);
-        addRequirements(endEffectorSubsystem);
-        addRequirements(armSubsystem);
+        
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        System.out.println("AutoAlignRightAutoCommand::initialize() called");
-        finished = true;
+        finished = false;
+        autoAlignRightCommand = new AutoAlignRightCommand();
+        scg = new SequentialCommandGroup(autoAlignRightCommand);
+        scg.schedule();
+        
+        //CommandScheduler.getInstance().schedule(scg);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
 
-        if(limelight.hasTarget()) {
-            aprilTagLocation = LimelightHelpers.getTX(Constants.LimelightConstants.name);
-
-            //System.out.println("aprilTagLocation: " + aprilTagLocation + " -Constants.DriveConstants.kAutoAlignOffset: " + (-Constants.DriveConstants.kAutoAlignOffset) + "Constants.DriveConstants.kAutoAlignTolerance")
-
-            if(
-                (aprilTagLocation ) > (Constants.DriveConstants.kAutoAlignRightOffset - Constants.DriveConstants.kAutoAlignTolerance)
-                && (aprilTagLocation) < (Constants.DriveConstants.kAutoAlignRightOffset + Constants.DriveConstants.kAutoAlignTolerance)
-            ) {
-                // We are in the zone
-                driveSubsystem.driveRobotRelative(0.0, 0.0, 0.0);
-
-                /*SequentialCommandGroup scg = new SequentialCommandGroup(
-                    new EjectCoralCommand(),
-                    new DriveBackwardsCommand(),
-                    new ArmStartCommand()
-
-                );
-
-                scg.execute();*/
-
-                // Set the LED to show that it has the target
-                RobotContainer.led1.setStatus(LEDStatus.targetAquired);
-                
-                if(
-                    armSubsystem.getDesiredState() == ArmState.CoralL1
-                    || armSubsystem.getDesiredState() == ArmState.CoralL2
-                ) {
-                    // We are in a CoralL1 or CoralL2 position, eject out the front
-                    endEffectorSubsystem.setDesiredState(EndEffectorState.EjectCoralFront);
-                    finished = true;
-                } else if(
-                    armSubsystem.getDesiredState() == ArmState.CoralL3
-                    || armSubsystem.getDesiredState() == ArmState.CoralL4
-                ) {
-                    // We are in a CoralL3 or CoralL4 position, eject out the back
-                    endEffectorSubsystem.setDesiredState(EndEffectorState.EjectCoralBack);
-                    finished = true;
-                }
-            } else if(
-                (aprilTagLocation) < (Constants.DriveConstants.kAutoAlignRightOffset + Constants.DriveConstants.kAutoAlignTolerance)
-            ) {
-                driveSubsystem.driveRobotRelative(0.0, -Constants.DriveConstants.kAutoAlignSpeed, 0.0);
-
-                // Set the LED to show that it has the target
-                RobotContainer.led1.setStatus(LEDStatus.targetSearching);
-            } else if (
-                (aprilTagLocation) > (Constants.DriveConstants.kAutoAlignRightOffset - Constants.DriveConstants.kAutoAlignTolerance)
-            ) {
-                driveSubsystem.driveRobotRelative(0.0, Constants.DriveConstants.kAutoAlignSpeed, 0.0);
-
-                // Set the LED to show that it has the target
-                RobotContainer.led1.setStatus(LEDStatus.targetSearching);
-            }
-        } else {
-            // we don't have a target to stop
-            driveSubsystem.driveRobotRelative(0.0, 0.0, 0.0);
-
-            // Set the LED to show that it has the target
-            RobotContainer.led1.setStatus(LEDStatus.targetSearching);
-        }
+        scg.execute();
     }
 
     // Called once the command ends or is interrupted.
@@ -112,7 +50,6 @@ public class AutoAlignRightAutoCommand extends Command  {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        System.out.println("AutoAlignRightAutoCommand::isFinished() called");
-        return finished;
+        return scg.isFinished();
     }
 }
